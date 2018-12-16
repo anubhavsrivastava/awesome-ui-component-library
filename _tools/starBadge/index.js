75b899d18272c6ca725f40b7c0b2df40302cdbc2
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-12-16 18:11:11 +0530

    feat: added support for copying to clipboard for starBadge tool
    Also moved it to dedicated folder

--- _tools/starBadge/index.js
+++ _tools/starBadge/index.js
@@ -1,4 +1,13 @@
 var arguments = process.argv.slice(2);
+
+function moduleAvailable(name) {
+	try {
+		require.resolve(name);
+		return true;
+	} catch (e) {}
+	return false;
+}
+
 if (!arguments.length) {
 	console.log('Need a GITHUB repo URL problem URL; eg. https://github.com/anubhavsrivastava/leetcodeJS ');
 	process.exit(0);
@@ -14,3 +23,10 @@ let badgeURL = `https://img.shields.io/github/stars/${repoArr[0]}/${repoArr[1]}.
 let content = `[![Repo Star](${badgeURL})](${arguments[0]})`;
 
 console.log('Badge Url: ', content);
+
+//Copying the result functionality would be only available if you choose to install package in this folder
+if (moduleAvailable('clipboardy')) {
+	// yeah we've got it!
+	const clipboardy = require('clipboardy');
+	clipboardy.writeSync(content);
+}
